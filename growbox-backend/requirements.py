2ab79660afc571c80c5cda48e3ffa2ef5f8b78cd
Christoph <ChristophSpiess@mytum.de>
2024-03-31 20:10:08 +0200

    update requirements

--- growbox-backend/requirements.py
+++ growbox-backend/requirements.py
@@ -1,7 +1,33 @@
 # File: requirements.py
 
-Flask==2.1.0
-Flask-Cors==3.0.10
-pymongo==3.12.0
-flask-socketio==4.3.0
+#old
+#Flask==2.1.0
+#Flask-Cors==3.0.10
+#pymongo==3.12.0
+#flask-socketio==4.3.0
 
+#new
+bcrypt==4.0.1
+bidict==0.22.1
+click==8.1.3
+colorama==0.4.6
+dnspython==2.3.0
+Flask==2.2.3
+Flask-Bcrypt==1.0.1
+Flask-Cors==3.0.10
+Flask-SocketIO==5.3.5
+h11==0.14.0
+itsdangerous==2.1.2
+Jinja2==3.1.2
+MarkupSafe==2.1.2
+paho-mqtt==1.6.1
+pip==23.2.1
+pymongo==4.3.3
+python-engineio==4.5.1
+python-socketio==5.8.0
+setuptools==3.3
+simple-websocket==0.10.1
+six==1.16.0
+websockets==12.0
+Werkzeug==2.3.6
+wsproto==1.2.0
