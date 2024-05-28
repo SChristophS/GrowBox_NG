f3e69c7d0e6419d1f5c0b852c7eac29ac1349395
Christoph <ChristophSpiess@mytum.de>
2024-05-28 23:29:54 +0200

    remove temp files

--- growbox-controller/STM32F1_GCU/Debug/Core/Src/w5500/wizchip_conf.d
+++
@@ -1,6 +0,0 @@
-Core/Src/w5500/wizchip_conf.o: ../Core/Src/w5500/wizchip_conf.c \
- ../Core/Src/w5500/wizchip_conf.h ../Core/Src/w5500/W5500/w5500.h \
- ../Core/Src/w5500/wizchip_conf.h
-../Core/Src/w5500/wizchip_conf.h:
-../Core/Src/w5500/W5500/w5500.h:
-../Core/Src/w5500/wizchip_conf.h:
